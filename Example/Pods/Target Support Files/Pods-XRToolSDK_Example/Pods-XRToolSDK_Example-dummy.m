4bd6dd71bd00b0efa4ddc24cc931d3462ea0751e
MissSunday <963217127@qq.com>
2021-08-30 10:06:37 +0800

    start

---
+++ Example/Pods/Target Support Files/Pods-XRToolSDK_Example/Pods-XRToolSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XRToolSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XRToolSDK_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-XRToolSDK_Tests/Pods-XRToolSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XRToolSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XRToolSDK_Tests
+@end
